a518674029fce90b5974871b20073a39ae1dda90
Tom Söderlund <tom@tomsoderlund.com>
2019-10-22 10:55:19 +0200

    Rename index page

--- server/routes.js
+++ server/routes.js
@@ -6,10 +6,11 @@ const routesImplementation = routes()
 //   .add('/blog/:slug', 'blogShow')
 //   .add('showBlogPostRoute', '/blog/:slug', 'blogShow')
 
-routesImplementation.add('/articles/:slug', 'articleList')
-routesImplementation.add('/articles', 'articleList')
-routesImplementation.add('/', 'articleList')
+routesImplementation.add('/articles/:slug', 'index')
+routesImplementation.add('/articles', 'index')
+routesImplementation.add('/', 'index')
 
 module.exports = routesImplementation
 
 // Usage inside Page.getInitialProps (req = { pathname, asPath, query } = { pathname: '/', asPath: '/about', query: { slug: 'about' } })
+// Note: query contains both /:params and ?query=value from url
