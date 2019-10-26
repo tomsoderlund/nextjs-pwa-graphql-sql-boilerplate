9fc62042ea050802c4609e454b6106bb0ff8627a
Tom Söderlund <tom@tomsoderlund.com>
2019-10-26 12:42:10 +0200

    apollo-server-micro

---
+++ api/graphql/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer, gql } = require('apollo-server-micro')
+const { config } = require('../../config/config')
+
+// Postgres (pg)
+const { Pool } = require('pg')
+const pool = new Pool({ connectionString: config.databaseUrl })
+
+console.log(`config.databaseUrl:`, config.databaseUrl)
+
+const typeDefs = gql`
+  ${require('../../graphql/article/schema')}
+`
+
+const resolvers = {
+  ...require('../../graphql/article/resolvers')(pool)
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+})
+
+module.exports = server.createHandler({ path: '/api/graphql' })
