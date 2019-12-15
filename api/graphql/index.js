e19f4cfbc4a9051a4ee0b73231fbe3a183d37568
Tom Söderlund <tom@tomsoderlund.com>
2019-12-15 16:52:42 +0100

    Improved database connection pooling (serverless)

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -3,7 +3,18 @@ const { config } = require('../../config/config')
 
 // Postgres (pg)
 const { Pool } = require('pg')
+const onProcessDeath = require('death')
 const pool = new Pool({ connectionString: config.databaseUrl })
+let client
+
+const initDatabaseClient = async () => {
+  if (!client) client = await pool.connect()
+}
+initDatabaseClient()
+
+onProcessDeath((signal, err) => {
+  client && client.release()
+})
 
 const typeDefs = gql`
   ${require('../../graphql/article/schema')}
