53aba9ad2d2d56fdd9a33637444d42b441ff00bd
Tom Söderlund <tom@tomsoderlund.com>
2020-02-09 14:47:17 +0100

    Sitemap as functional component

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,20 +1,32 @@
 import React from 'react'
+import ReactDOMServer from 'react-dom/server'
 
 import { config } from '../config/config'
 import formatDate from '../lib/formatDate'
 
-const getDate = () => formatDate(new Date())
-const pages = ['/']
-
-const sitemapXml = '<?xml version="1.0" encoding="UTF-8"?>' +
-  '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
-    pages.map(path => `<url><loc>${config.appUrl}${path.substr(1)}</loc><lastmod>${getDate()}</lastmod></url>`) +
-  '</urlset>'
-
-export default class Sitemap extends React.Component {
-  static getInitialProps ({ res }) {
-    res.setHeader('Content-Type', 'text/xml')
-    res.write(sitemapXml)
-    res.end()
-  }
+const Sitemap = () => {
+  const getDate = () => formatDate(new Date())
+  const pages = ['/']
+  return (
+    <urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>
+      {pages.map((path, index) => (
+        <url key={index}>
+          <loc>{config.appUrl}{path.substr(1)}</loc>
+          <lastmod>{getDate()}</lastmod>
+        </url>
+      ))}
+    </urlset>
+  )
 }
+
+Sitemap.getInitialProps = async ({ res }) => { // { req, res, pathname, asPath, query }
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(
+    ReactDOMServer.renderToStaticMarkup(
+      <Sitemap />
+    )
+  )
+  res.end()
+}
+
+export default Sitemap
