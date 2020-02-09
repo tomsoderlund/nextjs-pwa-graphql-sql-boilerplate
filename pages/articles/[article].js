86ee29e0a83ae3b5926bc787a8d981ff22cfc4c5
Tom Söderlund <tom@tomsoderlund.com>
2020-02-09 18:26:22 +0100

    Article page - handle loading better

--- pages/articles/[article].js
+++ pages/articles/[article].js
@@ -12,22 +12,22 @@ function ArticlePage ({ query }) {
   const { data, loading, error } = useGetArticle(query.article)
 
   if (error || (data && !data.article)) return <ErrorPage />
-  if (loading) return <div>Loading...</div>
 
-  return <Page title={data.article.title} >
-    <ArticleDetails
-      article={data.article}
-    />
+  return <Page
+    title={loading ? 'Loading...' : data.article.title}
+  >
+    {loading ? <div>Loading...</div> : (
+      <ArticleDetails
+        article={data.article}
+      />
+    )}
 
     <h2>Routing</h2>
     <p>Current query: <strong>{JSON.stringify(query)}</strong></p>
 
     <ul>
       <li><Link href='/'><a>Home</a></Link></li>
-      <li><Link href='/articles/other-1'><a>Other route</a></Link></li>
     </ul>
-
-    <p>Get the <a target='_blank' href='https://github.com/tomsoderlund/nextjs-pwa-graphql-sql-boilerplate'>source code for nextjs-pwa-graphql-sql-boilerplate</a></p>
   </Page>
 }
 
