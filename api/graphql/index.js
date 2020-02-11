807fa2c91b75a228cecc3fbbeab54eaedaffaf62
Tom Söderlund <tom@tomsoderlund.com>
2020-02-11 22:50:05 +0100

    Connect to database, do query, then release database connection

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -5,25 +5,26 @@ const { config } = require('../../config/config')
 
 // Postgres (pg)
 const { Pool } = require('pg')
-const onProcessDeath = require('death')
 const pool = new Pool({ connectionString: config.databaseUrl })
-let client
 
-const initDatabaseClient = async () => {
-  if (!client) client = await pool.connect()
+// Connect to database, do query, then release database connection
+const query = async (sqlString) => {
+  try {
+    const client = await pool.connect()
+    const results = await pool.query(sqlString)
+    client.release()
+    return results
+  } catch (error) {
+    return { error }
+  }
 }
-initDatabaseClient()
-
-onProcessDeath((signal, err) => {
-  client && client.release()
-})
 
 const typeDefs = mergeTypes([
   require('../../graphql/article/schema')
 ])
 
 const resolvers = mergeResolvers([
-  require('../../graphql/article/resolvers')(pool)
+  require('../../graphql/article/resolvers')({ query })
 ])
 
 const server = new ApolloServer({
