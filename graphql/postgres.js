99f5f1d4ef3f240a4cb68654821d5fc9c9021b34
Tom Söderlund <tom@tomsoderlund.com>
2020-02-24 09:51:21 +0100

    Avoid create new pg client

--- graphql/postgres.js
+++ graphql/postgres.js
@@ -7,9 +7,9 @@ const pool = new Pool({ connectionString: config.databaseUrl })
 // Connect to database, do query, then release database connection
 module.exports.query = async (sqlString) => {
   try {
-    const client = await pool.connect()
+    // const client = await pool.connect()
     const results = await pool.query(sqlString)
-    client.release()
+    // client.release()
     return results
   } catch (error) {
     return { error }
