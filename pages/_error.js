23c4b86fe949c86486875d1c05a0ff438a882e16
Tom Söderlund <tom@tomsoderlund.com>
2020-03-02 11:37:29 +0100

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -13,7 +13,11 @@ const ErrorPage = ({ statusCode }) => (
 )
 
 ErrorPage.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  const statusCode = res
+    ? res.statusCode
+    : err
+      ? err.statusCode
+      : 404
   return { statusCode }
 }
 
