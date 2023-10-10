005bc9f9c9f7a31f0af587aec42d9b8166bed763
tomsoderlund <tom@tomsoderlund.com>
2023-10-10 16:31:12 +0200

    Update /signin

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import type { GetStaticPropsResult } from 'next'
-import Image from 'next/image'
 import { Button } from '@mui/material'
 
+import { config } from 'config/config'
 import { useUser } from 'graphql/collections/user/hooks'
 import SigninWithEmailForm from 'components/user/SigninWithEmailForm'
 import SigninWithGoogleButton from 'components/user/SigninWithGoogleButton'
@@ -12,8 +12,7 @@ function SigninPage (): React.ReactElement {
   return user === null
     ? (
       <>
-        <Image src='/images/brizo_logo.svg' alt='Brizo logo' width={200} height={30} />
-        <h1>Welcome to the Brizo Dashboard</h1>
+        <h1>Welcome to {config.appName}</h1>
         <SigninWithGoogleButton redirectTo='/orders' />
         <p>or sign in with email:</p>
         <SigninWithEmailForm redirectTo='/orders' />
@@ -32,7 +31,6 @@ export default SigninPage
 
 export const getStaticProps = async (): Promise<GetStaticPropsResult<{}>> => ({
   props: {
-    title: 'Sign in',
-    layout: 'signin'
+    title: 'Sign in'
   }
 })
