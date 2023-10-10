7f5d492c1d594c32bcde0cfd6d6a1c8b9d4b3980
tomsoderlund-hm <tom.soderlund@hm.com>
2023-10-10 12:46:08 +0200

    Article schema

--- graphql/collections/article/resolvers.ts
+++ graphql/collections/article/resolvers.ts
@@ -1,38 +1,11 @@
-// Server-side
-const { sql: { sqlFind, sqlCreate, sqlUpdate, sqlDelete } } = require('sql-wizard')
+// For Postgraphile makeWrapResolversPlugin; see graphql/server/resolverExtensions.ts
 
-module.exports = (pool) => ({
-  Query: {
-    async articles (parent, variables, context, info) {
-      const rows = await sqlFind(pool, 'article', variables)
-      return rows
-    },
+import { ArticleInput } from 'graphql/__generated__/graphql'
 
-    async article (parent, variables, context, info) {
-      const id = variables.slugAndId
-        ? variables.slugAndId.split('-').pop()
-        : variables.id
-      const rows = await sqlFind(pool, 'article', { id })
-      return rows[0]
-    }
-  },
-
-  Mutation: {
-    async addArticle (parent, variables, context, info) {
-      const newArticle = await sqlCreate(pool, 'article', variables)
-      return newArticle
-    },
-
-    async updateArticle (parent, variables, context, info) {
-      const { id, ...values } = variables
-      await sqlUpdate(pool, 'article', { id }, values)
-      const rows = await sqlFind(pool, 'article', { id })
-      return rows[0]
-    },
-
-    async deleteArticle (parent, variables, context, info) {
-      await sqlDelete(pool, 'article', variables)
-      return variables
-    }
-  }
-})
+export const createArticle = async (resolve: any, source: any, args: any, context: any, resolveInfo: any): Promise<void> => {
+  // const articleInput: ArticleInput = args.input.article
+  // ...do something before save to local database
+  // Continue with the original resolver
+  const result = await resolve(source, args, context, resolveInfo)
+  return result
+}
