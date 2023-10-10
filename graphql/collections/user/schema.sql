302b52d5c5044968508408b5f950e696b2ccfde6
tomsoderlund <tom@tomsoderlund.com>
2023-10-10 16:28:17 +0200

    User schema

--- graphql/collections/user/schema.sql
+++ graphql/collections/user/schema.sql
@@ -1,12 +1,14 @@
 -- Table: user
--- Represents a Brizo Dashboard user who can log in, perform actions, and own data.
+-- Represents a user who can log in, perform actions, and own data.
 
 DROP TABLE IF EXISTS "user" CASCADE;
 
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
+    created_date timestamp with time zone NOT NULL DEFAULT now(),
+
     name text,
     firebase_uid varchar(128) NOT NULL UNIQUE,
-    created_date timestamp with time zone NOT NULL DEFAULT now(),
-    is_system_admin boolean NOT NULL DEFAULT false -- For the entire Brizo Dashboard, see also user_goodsowner.is_admin
+
+    is_system_admin boolean NOT NULL DEFAULT false
 );
