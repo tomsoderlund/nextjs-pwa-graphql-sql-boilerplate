27626ac9a6a94345a97e61e1aa7a0f87234e7e08
Tom Söderlund <tom@tomsoderlund.com>
2023-10-24 21:13:33 +0200

    Revert TypeScript fix on graphql/__generated__

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: [
-      'graphql/__generated__'
-    ]
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
