e7df60554cb4fa2ec94040d8c7e8e844d6ec1361
Tom Söderlund <tom@tomsoderlund.com>
2024-03-01 14:55:12 +0100

    all_tables

--- graphql/collections/all_tables.sql
+++ graphql/collections/all_tables.sql
@@ -2,24 +2,8 @@
 -- \i graphql/collections/roles/roles.sql
 
 -- Tables
-\i graphql/collections/goodsowner/schema.sql
 \i graphql/collections/user/schema.sql
-\i graphql/collections/user_goodsowner/schema.sql
-
-\i graphql/collections/product/schema.sql
-
-\i graphql/collections/transporter/schema.sql
-\i graphql/collections/transporter_service/schema.sql
-
-\i graphql/collections/customer/schema.sql
-
-\i graphql/collections/order/schema.sql
-\i graphql/collections/order_product/schema.sql
-
-\i graphql/collections/supplier/schema.sql
-
-\i graphql/collections/purchaseorder/schema.sql
-\i graphql/collections/purchaseorder_product/schema.sql
+\i graphql/collections/article/schema.sql
 
 -- Functions (see also each schema.sql file)
 
